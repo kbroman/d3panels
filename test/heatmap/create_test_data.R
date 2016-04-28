2ab5a4f4792cd37ed7b8097de8ffd0aa7eff4abd
Karl Broman <kbroman@gmail.com>
2016-04-28 14:28:16 -0500

    Add heatmap test with unequally spaced bins

--- test/heatmap/create_test_data.R
+++ test/heatmap/create_test_data.R
@@ -9,3 +9,17 @@ for(i in seq(along=x))
 
 cat(jsonlite::toJSON(list(x=x, y=y, z=z)),
     file="data.json")
+
+# second data set with unequal bins
+set.seed(40092090)
+nx <- 101
+ny <- 51
+x <- sort(runif(nx, -pi, pi))
+y <- sort(runif(ny, -pi/2, pi/2))
+z <- matrix(nrow=nx, ncol=ny)
+for(i in seq(along=x))
+    for(j in seq(along=y))
+        z[i,j] <- sin(x[i]) + cos(y[j])
+
+cat(jsonlite::toJSON(list(x=x, y=y, z=z)),
+    file="data_unequal.json")
--- test/heatmap/test_heatmap.coffee
+++ test/heatmap/test_heatmap.coffee
@@ -6,5 +6,9 @@ w = 600
 # Example: simplest use
 d3.json "data.json", (data) ->
     mychart = heatmap({height:h, width:w, zthresh:0.5})
+    mychart(d3.select("div#chart1"), data)
 
-    mychart(d3.select("div#chart"), data)
+# Example: unequal bins
+d3.json "data_unequal.json", (data) ->
+    mychart2 = heatmap({height:h/2, width:w, zthresh:0.5})
+    mychart2(d3.select("div#chart2"), data)
