751797b425b7fe35111db390a65ad513e68aaebc
Karl Broman <kbroman@gmail.com>
2016-05-05 23:39:23 -0500

    Had forgotten to force-add the d3panels_top and _bottom.js files

---
+++ src/d3panels_bottom.js
@@ -0,0 +1,3 @@
+    if (typeof define === "function" && define.amd) this.d3panels = d3panels, define(d3panels); else if (typeof module === "object" && module.exports) module.exports = d3panels; else this.d3panels = d3panels;
+
+}(); // bottom of d3panels encapsulation
---
+++ src/d3panels_top.js
@@ -0,0 +1,4 @@
+!function() { // encapsulate d3panels functions
+    var d3panels = {
+        version: "1.0.0"
+    };
