f11488e0ee58da889f86cb2ecedd2a9576f4e9c0
Karl Broman <kbroman@gmail.com>
2026-06-02 19:07:10 -0500

    add linked charts in test of timeplot()

--- assets/test/timeplot/test_timeplot.js
+++ assets/test/timeplot/test_timeplot.js
@@ -5,31 +5,18 @@
   // illustration of use of the timeplot function
 
   // example 1
-  var h, halfh, halfw, margin, totalh, totalw, w;
-
-  h = 550;
-
-  w = 900;
-
-  margin = {
-    left: 60,
-    top: 40,
-    right: 40,
-    bottom: 40,
-    inner: 5
-  };
-
-  halfh = h + margin.top + margin.bottom;
-
-  totalh = halfh * 2;
-
-  halfw = w + margin.left + margin.right;
-
-  totalw = halfw * 2;
-
   // Example 1: simplest use
   d3.json("data.json").then(function (data) {
-    var d, mychart, these_data;
+    var d, h, margin, mychart, these_data, w;
+    h = 550;
+    w = 900;
+    margin = {
+      left: 60,
+      top: 40,
+      right: 40,
+      bottom: 40,
+      inner: 5
+    };
     mychart = d3panels.timeplot({
       xlab: "Date",
       ylab: "Download speed",
@@ -79,9 +66,18 @@
     });
   });
 
-  // Example 2: sample, but less data, so x-axis has times instead of dates
+  // Example 2: same, but less data, so x-axis has times instead of dates
   d3.json("data.json").then(function (data) {
-    var i, mychart, n_pts, start, these_data;
+    var h, i, margin, mychart, n_pts, start, these_data, w;
+    h = 550;
+    w = 900;
+    margin = {
+      left: 60,
+      top: 40,
+      right: 40,
+      bottom: 40,
+      inner: 5
+    };
     mychart = d3panels.timeplot({
       xlab: "Time",
       ylab: "Download speed",
@@ -133,6 +129,150 @@
       return d3.select(this).attr("r", 3 * 3);
     }).on("mouseout", function () {
       return d3.select(this).attr("r", 3);
+    });
+  });
+
+  // Example 3: linked charts
+  d3.json("data.json").then(function (data) {
+    var chart1, chart2, d, data1, data2, h, halfh, hilitcolor, margin, mychart1, mychart2, pointcolor, r, svg, totalh, totalw, w;
+    pointcolor = "slateblue";
+    hilitcolor = "pink";
+    h = 450;
+    w = 900;
+    margin = {
+      left: 60,
+      top: 10,
+      right: 40,
+      bottom: 20,
+      inner: 5
+    };
+    halfh = h + margin.top + margin.bottom;
+    totalh = halfh * 2;
+    totalw = w + margin.left + margin.right;
+    r = 3;
+    mychart1 = d3panels.timeplot({
+      xlab: "Time",
+      ylab: "Download speed",
+      height: h,
+      width: w,
+      pointsize: r,
+      pointcolor: pointcolor,
+      ylim: [0, 1000],
+      margin: margin
+    });
+    mychart2 = d3panels.timeplot({
+      xlab: "Time",
+      ylab: "Upload speed",
+      height: h,
+      width: w,
+      pointsize: r,
+      pointcolor: pointcolor,
+      ylim: [0, 1000],
+      margin: margin
+    });
+    // reorg data
+    data1 = {
+      x: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(d.time);
+        }
+        return results;
+      }(),
+      y: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(d.download);
+        }
+        return results;
+      }(),
+      indID: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(Math.round(d.download) + " Mbps (" + d3panels.formatdatetime(new Date(d.time)) + ")");
+        }
+        return results;
+      }()
+    };
+    data2 = {
+      x: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(d.time);
+        }
+        return results;
+      }(),
+      y: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(d.upload);
+        }
+        return results;
+      }(),
+      indID: function () {
+        var j, len, results;
+        results = [];
+        for (j = 0, len = data.length; j < len; j++) {
+          d = data[j];
+          results.push(" " + Math.round(d.upload) + " Mbp (" + d3panels.formatdatetime(new Date(d.time)) + ")");
+        }
+        return results;
+      }()
+    };
+    svg = d3.select("div#chart3").append("svg").attr("class", "d3panels").attr("height", totalh).attr("width", totalw);
+    chart1 = svg.append("g").attr("id", "chart01");
+    chart2 = svg.append("g").attr("id", "chart02").attr("transform", "translate(0," + halfh + ")");
+    // make plots
+    mychart1(chart1, data1);
+    mychart2(chart2, data2);
+    // animate points
+    mychart1.points().on("mouseover", function (d, i) {
+      d3.select(this).attr("r", r * 3).attr("fill", hilitcolor);
+      return chart2.selectAll("circle").attr("fill", function (d2, i2) {
+        if (i2 === i) {
+          return hilitcolor;
+        } else {
+          return pointcolor;
+        }
+      }).attr("r", function (d2, i2) {
+        if (i2 === i) {
+          return r * 3;
+        } else {
+          return r;
+        }
+      });
+    }).on("mouseout", function (d, i) {
+      d3.select(this).attr("r", r).attr("fill", pointcolor);
+      return chart2.selectAll("circle").attr("fill", pointcolor).attr("r", r);
+    });
+    return mychart2.points().on("mouseover", function (d, i) {
+      d3.select(this).attr("r", r * 3).attr("fill", hilitcolor);
+      return chart1.selectAll("circle").attr("fill", function (d2, i2) {
+        if (i2 === i) {
+          return hilitcolor;
+        } else {
+          return pointcolor;
+        }
+      }).attr("r", function (d2, i2) {
+        if (i2 === i) {
+          return r * 3;
+        } else {
+          return r;
+        }
+      });
+    }).on("mouseout", function (d, i) {
+      d3.select(this).attr("r", r).attr("fill", pointcolor);
+      return chart1.selectAll("circle").attr("fill", pointcolor).attr("r", r);
     });
   });
 }).call(undefined);
